d3b5549f4b5b319261c6f50c5c80123b0ab50195
ios7jbpro <71101500+ios7jbpro@users.noreply.github.com>
2024-06-13 13:34:21 +0300

    Add files via upload

---
+++ checksptlrx.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+shopt -s xpg_echo
+
+echo "Spotify cookie for authorization not detected."
+echo "    1. Open your browser."
+echo "    2. Press F12, open the Network tab and go to open.spotify.com."
+echo "    3. Click on the first request to open.spotify.com."
+echo "    4. Scroll down to the Request Headers, right click the cookie field and select Copy value."
+echo "    5. When you have the cookie copied, press enter to proceed."
+echo "       This will open the config file in the nano text editor. Go to the sptlrx section."
+echo "       At the end, add '--cookie PASTEYOURCOOKIEHERE'."
+
+read -p "Press enter to continue..."
+
+./splyrics.sh -e
+
