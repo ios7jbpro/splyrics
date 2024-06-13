dd4a4f50fddefc9b5ebb17cd0053925796731880
ios7jbpro <71101500+ios7jbpro@users.noreply.github.com>
2024-06-13 15:44:44 +0300

    Update checksptlrx.sh

--- checksptlrx.sh
+++ checksptlrx.sh
@@ -8,10 +8,9 @@ echo "    2. Press F12, open the Network tab and go to open.spotify.com."
 echo "    3. Click on the first request to open.spotify.com."
 echo "    4. Scroll down to the Request Headers, right click the cookie field and select Copy value."
 echo "    5. When you have the cookie copied, press enter to proceed."
-echo "       This will open the config file in the nano text editor. Go to the sptlrx section."
-echo "       At the end, add '--cookie PASTEYOURCOOKIEHERE'."
+echo "       This will open the config file in the nano text editor. Go to the sptlrx-cookie section."
+echo "       Append your cookie value here, save, leave, re-run the script."
 
 read -p "Press enter to continue..."
 
 ./splyrics.sh -e
-
