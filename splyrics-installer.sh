88f197cd43c130f4f5f5914e486f26733168bf77
ios7jbpro <bariskirmizi.208@gmail.com>
2024-06-23 23:15:39 +0300

    NixOS Check

--- splyrics-installer.sh
+++ splyrics-installer.sh
@@ -1,4 +1,12 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+# Check the Linux distribution
+DISTRO=$(grep '^ID=' /etc/os-release | cut -d'=' -f2)
+
+if [ "$DISTRO" == "nixos" ]; then
+  echo "NixOS detected. PLEASE DO NOT TRY TO INSTALL THIS SCRIPT SYSTEMWIDE ON NIXOS."
+  exit 1
+fi
 
 # Define source and destination paths
 SOURCE_SCRIPT="$(realpath splyrics.sh)"
@@ -17,3 +25,4 @@ sudo chmod +x "$DESTINATION_SCRIPT"
 echo "SpLyrics installed successfully."
 echo -e "\e[1m\e[91mWARNING: DO NOT DELETE THE ORIGINAL DIRECTORY WHERE splyrics.sh IS LOCATED.\e[0m"
 echo "You can now run the script using 'splyrics'."
+
